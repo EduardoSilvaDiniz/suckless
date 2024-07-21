ff01d65b635b3bed9f4bf508ad5b547d48a5a752
Eduardo Silva <eduardofox989@protonmail.com>
2024-07-21 19:47:56 -0300

    add script autostart

---
+++ .dwm/autostart.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# este arquivo deve ficar dentro do diretorio .dwm na home do usuario. /home/$homeUser/.dwm/autostart.sh
+dwmblocks &
+redshift -l -23.614138:-46.757899 &
+xcompmgr &
+feh --bg-scale ~/Pictures/wallpaper.png &
